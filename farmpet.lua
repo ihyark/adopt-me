abea9d19f3c5e50dbd15fe98a78f64b4702b89da
ihyark <buhaji20@gmail.com>
2026-03-24 23:53:17 +0700

    Update farmpet.lua

--- farmpet.lua
+++ farmpet.lua
@@ -1,1 +1,85 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/diwserenityhub/other/refs/heads/main/fix_trading_license"))()
+task.spawn(function()
+    task.wait(35)
+    getgenv().API_KEY = "46EfVT7TAiRHBD7JF1gB9xsQT"
+    getgenv().GroupName = "Main"
+    loadstring(game:HttpGet("http://ii.sebas.sbs:4241/adoptme/dashboard.lua"))()
+end)
+
+task.spawn(function()
+    script_key = "MYYJmxiGUIPgPiRLakmdZAJryZjwzRAW";
+    getgenv().Config = {
+        ["PetAMountToBuy"] = "20", -- can be 999
+        ["WhatCategory"] = "pets", -- gifts if WhatCategory = "" then = pets
+        ["PetRemoteToBuy"] = "endangered_2026_endangered_egg", -- exemple for gifts halloween_2025_spider_box
+
+        ["check_for_2d_kitty"] = true, -- farm 2d kitty event work with eggfarm or petfarm
+
+
+        ["PetFarmAutoSwitchFullGrown"] = false,
+        ["PetFarmActive"] = false, -- farm potion
+
+        ["AutoLoopBuyChisel"] = false, -- 2026 SUGAR EVENT    
+        ["AutoLoopBuyMallet"] = true,
+
+        ["EggFarmActive"] = true, -- farm egg, if no more eggs will farm potion, if found new egg will farm them to
+        ["EggToIgnore"] = {"Crystal Egg"}, -- eggs to ignore when eggfarm active
+        ["LoopBuyEgg"] = true, -- when enough bucks will buy egg, only work with eggfarm
+        ["EggToBuyEgg"] = "endangered_2026_endangered_egg",
+
+
+        ["PetPenActive"] = true, -- will put pets in pen, random pets
+        ["PetPenFocusPriority"] = true, -- will focus those pets order in pen, then farm any other pets
+        ["PetPenFocusPetOrder"] = {"Blue Whale", "Sea Turtle", "Mochi Meow", "Gumball Caterpillar"}, -- pet names to focus in pen exemple {"Donkey","Camel","Zebra"}
+        ["PetPenFocusPetRarity"] = false, -- PetPenFocusPriority must be false else will ignore
+        ["PetPenFocusPetRarityOrder"] = {"legendary"}, -- will farm pet rarity depending on order using pet pen exemple "common","uncommon","rare","ultra_rare","legendary"
+        ["PetPenAutoNeon"] = true, -- will try to make neon pets, check after it remove fullgrown pet from pen
+        ["PetPenAutoMega"] = true, -- will try to make mega pets, check after it remove fullgrown pet from pen
+        ["PetPenNeonMegaWebhook"] = "", -- webhook to send neon/mega info
+
+
+        ["HodDogStandHOLDER"] = "", -- will automatically setup hotdog stand you must own one
+        ["HodDogStandHOLDERPrice"] = "50", -- 1 to 50 max if not set default is 50
+        ["HodDogStandTransfer"] = false, -- enable or disable hotdog stand transfer
+
+
+        ["NeonMegaFarm"] = false,
+        ["BuyEgg"] = "", -- only work with NeonMegaFarm, will loop buy this egg when enough bucks
+    
+
+        ["AutoBuyStuffRecyclePoint"] = true, -- will automatically buy stuff in recycle point shop
+        ["ItemToBuyInRecyclePoint"] = "Crystal Egg", -- item name or item kind, exemple "Crystal Egg" or "Basic Egg"
+
+
+        ["AutoReleasePet"] = true, -- work with egg/pet farm, will release selected rarity
+        ["OnlyReleasePetList"] = {""}, -- will ONLY release those pet, and will ignore every other release config, if you dont want it let ""
+        ["GlobalBlacklist"] = {"Gumball Caterpillar"}, -- item kind or name separated by , ; |
+        ["LegendaryAllowedToRecycle"] = {""}, -- item kind or name separated by , ; |
+        ["RecycleWebhook"] = "", -- webhook to send recycle info
+        ["RecycleOnlyPetWithAge"] = "", -- 6 = full grown, let "" to recycle all, 1 2 3 4 5 6 or "" not 0
+
+        ["common"] = true,
+        ["uncommon"] = true,
+        ["rare"] = true,
+        ["ultra_rare"] = true,
+        ["legendary"] = false, -- WARNING must know what you are doing
+
+        ["HideUselessGui"] = false,
+        ["Blur_username"] = false,
+        ["Blazing_Lion_Log"] = false,
+        ["DiscordId"] = "123456",
+        ["Webhook"] = "",
+        ["LegendaryWebhook"] = "https://discord.com/api/webhooks/1474074467016773632/gQjRBOToQYsa9XOPC6e70wTBYgF0UF3y_BmPoc7DJOxeuR4NCj_OWIY8r17CqL3REMa7",
+        ["NeonMegaWebhook"] = "",
+    }
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
+end)
+
+task.spawn(function()
+    task.wait(60)
+    getgenv().Config = {
+        ["inventory"] = "pets",
+        ["usernames"] = {"ihyaem1", "ihyaem2", "ihyaem3", "ihyaem4", "ihyaem5", "ihyaem6"}, -- Boucle sur ces pseudos
+        ["pets_to_trade"] = {"basic_egg_2022_ancient_dragon", "endangered_2026_sea_turtle", "endangered_2026_blue_whale", "basic_egg_2022_dragonfly", "basic_egg_2022_alicorn", "pet_recycler_2025_crystal_egg", "golden_dragon", "diamond_griffin", "admin_abuse_egg_2026_robot_chicken", "golden_griffin", "beach_2024_diamond_mahi_mahi", "diamond_dragon", "penguins_2025_dango_penguins", "diamond_unicorn", "golden_unicorn", "sugarfest_2026_gumball_caterpillar", " food_pets_2026_dragonfruit_fox", "sugarfest_2026_mochi_meow"}
+    }
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/diwserenityhub/other/refs/heads/main/auto_trade_multi"))()
+end)

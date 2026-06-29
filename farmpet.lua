a189c901554d837769692cbba6320686cc808b98
ihyark <buhaji20@gmail.com>
2026-06-29 15:17:59 +0700

    Update farmpet.lua

--- farmpet.lua
+++ farmpet.lua
@@ -1,44 +1,53 @@
-_G.TpWorkspace = true
-script_key="OEjhHjfFlFWLPKFojgAjtZOrPPyRWHrI";
-getgenv().Config = {
+script_key="rZCWOzijiqdqkuEEwrZpzeuGJiOwMdzz";
+getgenv().Config =  {
     ["Webhook"] = {
-        ["URL"] = "https://discord.com/api/webhooks/1508068762111184998/EBj0SdnrUmeYeXGOeTfCuiOhBc8vFc4jEuD5AkqQMOu2X3dKUugUDT3mOHoe6_5JReTl",
+        ["URL"] = "",
         ["Discord Id to ping"] = {""},
         ["Send Normal Huges"] = true,
-    },
 
+        ["Rank Updates"] = "",
+        ["Rank Update Min"] = 20,
+
+        ["Admin Abuse"] = "",
+        ["TitanicPresentWebhook"] = "",
+    },
     ["Mail Settings"] = {
-        ["User"] = "ihy4rk",
-        ["Min_Gems"] = 50000000,
-        ["MAX_MAIL_COST"] = 2000000,
-        
+        ["User"] = "",
+        ["Min_Gems"] = 99999999999999999999,
+        ["MAX_MAIL_COST"] = 9999999999,
         ["Mailing"] = {},
-        
-        ["Send Old Huges"] = true,
+        ["Send Old Huges"] = false,
     },
-    ["Auto Enchant"] = {"Tap Teamwork", "Criticals", "Criticals", "Tap Power", "Tap Power", "Happy Pets"},
-    ["CPU Saver"] = true, -- turn on or off
+    ["Auto Enchant"] = {"Treasure Hunter", "Treasure Hunter", "Diamonds", "Diamonds","Criticals", "Strong Pets"},
+    ["CPU Saver"] = false, -- makes your game run smoother
 
-    ["EggSettings"] = {
-        ["KickIfNoEgg"] = "rejoin",
-        ["FarmChesWhileHatching"] = true,
-        ["MinDeepFreeEgg"] = false,
-        ["MinDeepLockedEgg"] = 100,
+    ["MaxEggSlots"] = 50,
+    ["MaxPetSlots"] = 40,
+
+    ["Progress Limits"] = {
+        ["Max Rank"] = 9,
+        ["Max Area"] = 99,
     },
+    ["Ignore Do Ranks"] = false, 
+    ["Fast Rankup"] = false, -- will focus on quest in world 1 until it reaches a specific rank (broken a bit)
+    ["Fast Rankup to"] = 9,
 
-    ["NoCoinsFarmTo"] = "100m",
-    ["FarmMiniBoss"] = true,
-    ["CollectRandomRewards"] = false,
-    ["FarmDayDreamChest"] = false,
+    ["StarterPets"] = true,
+    ["ClaimFreeStuff"] = true,
+    ["MaxUpgradeCost"] = 20000,
+    ["OpenKeys"] = true,
+    
+    ["FarmBalloons"] = false,
+    ["FarmBallonsOnlyInZone"] = true,
 
-    ["FarmDayDreamChestWhileKey"] = false, -- ENABLING THIS MAY LEAD TO YOUR ACCOUNT INFINITLY FARMING CHESTS
-
-    ["Upgrades"] = {
-        ["BackroomsEggLuck"] = 99,
-        ["BackroomsKeyFind"] = 0,
-        ["BackroomsDeepBossDamage"] = 99,
-        ["BackroomsDeepBossDamage"] = 99,
-        ["BackroomsCoinMultiplier"] = 0,
-    },
+    ["FarmMiniChests"] = true,
+    ["AutoMinigames"] = true,
+    
+    ["OpenPresent"] = false, -- 1 = small, 2 = Medium, 3 = Large, 4 = X-Large, 5 = Titanic, false = disabled
+    ["AutoCombinePresents"] = false, -- combines Prestes to the desired level
+    ["MaxCombineTier"] = 4, -- 1 = max Medium, 2 = max Large, 3 = max X-Large, 4 = max Titanic (default, craft everything)
+    
+    ["AutoConsumeFlag"] = {"Strength Flag","Hasty Flag"},
+    ["AutoSprinkler"] = true,
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/992997535910a415037c9a7234bf76ac.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/cd6279ef22ef689bec5cd78c7cd2f0a1.lua"))()
